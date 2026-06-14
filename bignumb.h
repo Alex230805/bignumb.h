2513345fc5dc974f69256b509b8adfd47cc11520
Alex2300 <misuralealessandro@gmail.com>
2026-06-15 00:54:52 +0200

    adding more logic functions and finishing addition unit, adding multiplication functions

--- bignumb.h
+++ bignumb.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 #define NUMBER_UNIT int8_t 
 #define DEFAULT_NUMB_SIZE 8
@@ -18,15 +19,26 @@ typedef struct{
 	bool sign; // if true = negative number, if false = positive number
 }Int;
 
+void print_int(Int *a);
 void new_int_from_dec(Int* d, char* string_number);
 void new_int_from_hex(Int* d, char* string_number);
 void add_int(Int* res, Int* a,Int* b);
-void sub_int(Int* a,Int* b);
-void mux_int(Int* a,Int* b);
-void print_int(Int *a);
+void sub_int(Int* res, Int* a,Int* b);
+void mux_int(Int* res,Int* a,Int* b);
+bool int_eq(Int* a, Int* b);
+bool int_ueq(Int* a, Int* b);
+bool int_gtr(Int* a, Int* b);
+bool int_lst(Int* a, Int* b);
+void inc_int(Int* a);
+void dec_int(Int* a);
+void generate_int(Int* dest, int digits);
+
 
 NUMBER_UNIT int_pop_last(Int *a);
 void int_append_head(Int* a, NUMBER_UNIT n);
+void flip_int(Int* dest, Int* a);
+void clone_int(Int* dest, Int* a);
+void int_norm(Int* a, size_t len);
 
 #ifdef BIGNUMB_IMP
 
@@ -65,6 +77,10 @@ void new_int_from_hex(Int* d, char* string_number){
 }
 
 void print_int(Int *a){
+	if(a->tracker == 0){
+		printf("empty number\n");
+		return;
+	}
 	if(a->sign) printf("-");
 	for(size_t i=0;i<a->tracker; i++){
 		printf("%.2u", a->number[i]);
@@ -74,42 +90,119 @@ void print_int(Int *a){
 
 
 void add_int(Int* res, Int* a,Int* b){
-	size_t cached_a_tracker = a->tracker;
-	size_t cached_b_tracker = b->tracker;
+	Int ca = {0};
+	Int cb = {0};
+	clone_int(&ca, a);
+	clone_int(&cb, b);
+	bool inv_required = false;
 
 	NUMBER_UNIT carry = 0;
 	size_t d = 0;
-	if(a->tracker > b->tracker){
-		d = a->tracker;
+	if(ca.tracker > cb.tracker){
+		d = ca.tracker;
 	}else{
-		d = b->tracker;
+		d = cb.tracker;
 	}
+	if(ca.sign){
+		Int n = {0};
+		flip_int(&n, &ca);
+		memcpy(&ca, &n, sizeof(Int));
+		inv_required = true;
+	}
+	if(cb.sign){
+		Int m = {0};
+		flip_int(&m, &cb);
+		memcpy(&cb, &m, sizeof(Int));
+		inv_required = true;
+	}
+
 	for(size_t i=0;i<d || carry != 0; i++){
-		NUMBER_UNIT x = int_pop_last(a);
-		NUMBER_UNIT y = int_pop_last(b);
-		//printf("adding x=%.2d + y=%.2d\n", x, y);
-		int dst = 0;
-		dst = x+y+carry;
-		//printf("result = %d, carry = %d\n", dst, carry);
+		NUMBER_UNIT x = int_pop_last(&ca);
+		NUMBER_UNIT y = int_pop_last(&cb);
+		int dst = x+y+carry;
 		if(dst > 99){
 			carry = (int)(dst/100);
 			dst = dst%100;
+			if(i+1 == d && inv_required && carry){
+				// carry act as a flag, if is 1 then the final math with a 
+				// ten's complement number is positive and the carry must be 
+				// set to zero.
+				// If the carry is 0 the math with a ten's complement number 
+				// is negative. 
+				// If both number are negative ( so inverted with ten's complement )
+				// then the invertion is required by default no matter the carry outcome.
+				carry = 0; 
+				inv_required = false;
+			}
 		}else{
 			carry = 0;
 		}
 		int_append_head(res, dst);
 	}
-	a->tracker = cached_a_tracker;
-	b->tracker = cached_b_tracker;
+	if(inv_required || (a->sign && b->sign)){
+		Int j = {0}; 
+		clone_int(&j, res);
+		flip_int(res, &j);
+	}
+}
+
+void inc_int(Int* a){
+	Int factor = {0};
+	Int b = {0};
+	int_append_head(&factor, 1);
+	clone_int(&b, a);
+	a->tracker = 0;
+	add_int(a, &b, &factor);
+}
+
+void dec_int(Int* a){
+	Int factor = {0};
+	Int b = {0};
+	int_append_head(&factor, 1);
+	factor.sign = true;
+	clone_int(&b, a);
+	a->tracker = 0;
+	add_int(a, &b, &factor);
 }
 
 
-void sub_int(Int* a,Int* b){
-
+void sub_int(Int* res, Int* a,Int* b){
+	Int j = {0}; 
+	clone_int(&j, b);
+	flip_int(b, &j);
+	add_int(res, a, b);
 }
 
-void mux_int(Int* a,Int* b){
-
+void mux_int(Int* res, Int* a,Int* b){
+	Int i = {0};
+	Int m = {0};
+	Int ca = {0};
+	Int cb = {0};
+	res->tracker = 0;
+	clone_int(&ca, a);
+	clone_int(&cb, b);
+	int_append_head(&i, 0);
+	if(int_ueq(a, &i) || int_ueq(b, &i)){
+		int_append_head(res, 0);
+		return;
+	}
+	clone_int(&m, a);
+	dec_int(b);
+	dec_int(b);
+	while(int_lst(&i, b)){
+		add_int(res, a, &m);
+		clone_int(a, res);
+		res->tracker = 0;
+		inc_int(&i);
+	}
+	clone_int(res, a);
+	clone_int(a, &ca);
+	clone_int(b, &cb);
+	if(a->sign == b->sign) {
+		res->sign = false;
+	}else{
+		res->sign = true;
+	}
 }
 
 NUMBER_UNIT int_pop_last(Int *a){
@@ -140,6 +233,106 @@ void int_append_head(Int* a, NUMBER_UNIT n){
 	}
 	a->number[0] = n;
 	a->tracker += 1;
+}
+
+void flip_int(Int* dest, Int* a){
+	dest->tracker = 0;
+	for(size_t i=0;i<a->tracker; i++){
+		a->number[i] = 99-a->number[i];
+	}
+	Int b = {0};
+	new_int_from_dec(&b, "1");
+	a->sign = false;
+	add_int(dest, a, &b);
+	dest->sign = !a->sign;
+}
+
+void clone_int(Int* dest, Int* a){
+	dest->tracker = a->tracker;
+	dest->size = a->size;
+	dest->sign = a->sign;
+	dest->number = (NUMBER_UNIT*)malloc(sizeof(NUMBER_UNIT)*dest->size);
+	for(size_t i=0;i<dest->tracker; i++){
+		dest->number[i] = a->number[i];
+	}
+}
+
+bool int_eq(Int* a, Int* b){
+	bool res = false;
+	if((a->tracker == b->tracker) && (a->sign && b->sign)){
+		res = true;
+		for(size_t i=0;i<a->tracker;i+=1){
+			if(a->number[i] != b->number[i]){
+				res = false;
+				break;
+			}
+		}
+	}
+	return res;
+}
+bool int_ueq(Int* a, Int* b){
+	bool res = false;
+	if(a->tracker == b->tracker){
+		res = true;
+		for(size_t i=0;i<a->tracker;i+=1){
+			if(a->number[i] != b->number[i]){
+				res = false;
+				break;
+			}
+		}
+	}
+	return res;
+
+}
+
+bool int_gtr(Int* a, Int* b){
+	bool res = false;
+	if(!a->sign && b->sign){
+		return true;
+	}else if(a->sign && !b->sign){
+		return false;
+	}else{
+		size_t len = 0;
+		if(a->tracker > b->tracker){
+			len = a->tracker;
+		}else{
+			len = b->tracker;
+		}
+		int_norm(a, len);
+		int_norm(b, len);
+		for(size_t i=0;i<len;i+=1){
+			if(a->number[i] > b->number[i]){
+				res = true;
+				break;
+			}
+			if(a->number[i] < b->number[i]){
+				res = false;
+				break;
+			}
+		}
+		if(a->sign && b->sign){
+			res = !res;
+		}
+	}
+	return res;
+}
+
+bool int_lst(Int* a, Int* b){
+	return !int_gtr(a, b);
+}
+
+void int_norm(Int* a, size_t len){
+	if(a->tracker == len)	return;
+	for(size_t i=0;i<len-a->tracker; i+=1){
+		int_append_head(a, 0);
+	}	
+}
+
+void generate_int(Int* dest, int digits){
+	srand(time(NULL));
+	for(size_t i=0;i<digits/DIGIT_PER_UNIT; i++){
+		int_append_head(dest, rand()&99);
+	}
 }
 
 #endif // BIGNUMB_IMP
--- test.c
+++ test.c
@@ -7,8 +7,16 @@
 int main(){
 	Int a = {0};
 	Int b = {0};
-	new_int_from_dec(&a, "69000");
-	new_int_from_dec(&b, "4200");
+	new_int_from_dec(&a, "-11");
+	new_int_from_dec(&b, "-11");
+
+	printf("Testing logic comparision\n");
+	if(int_gtr(&a, &b)){
+		printf("A is greater than B\n");
+	}else{
+		printf("B is greater than A\n");
+	}
+	printf("Testing addition unit\n");
 	print_int(&a);
 	printf("+\n");
 	print_int(&b);
@@ -17,5 +25,34 @@ int main(){
 	add_int(&res, &a, &b);
 	print_int(&res);
 
+	printf("Testing random number generation\n");
+	Int random = {0};
+	generate_int(&random,300);
+	print_int(&random);
+
+	printf("Testing multiplication\n");
+	a.tracker = 0;
+	a.sign = false;
+	b.tracker = 0;
+	b.sign = false;
+	res.tracker = 0;
+	res.sign = false;
+	int_append_head(&a, 1);
+	int_append_head(&b, 20);
+	print_int(&a);
+	printf("*\n");
+	print_int(&b);
+	printf("=\n");
+	mux_int(&res, &a, &b);
+	print_int(&res);
+
+	printf("Testing more extreme multiplication\n");
+	a.tracker = 0;
+	generate_int(&a, 300);
+	b.tracker = 0;
+	generate_int(&a, 500);
+	res.tracker = 0;
+	mux_int(&res, &a, &b);
+	print_int(&res);
 	return 0;
 }
